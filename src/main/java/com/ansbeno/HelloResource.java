b75214a8778c4a88a025ec6a5aa1992d249d58c4
Anas BenOmar <138506861+AnsBenO@users.noreply.github.com>
2025-08-28 22:53:09 +0100

    using template extensions

--- src/main/java/com/ansbeno/HelloResource.java
+++ src/main/java/com/ansbeno/HelloResource.java
@@ -1,6 +1,9 @@
 package com.ansbeno;
 
+import java.math.BigDecimal;
+
 import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateExtension;
 import io.quarkus.qute.TemplateInstance;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -9,7 +12,9 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 @Path("/hello")
 public class HelloResource {
@@ -23,6 +28,14 @@ public class HelloResource {
             public static native TemplateInstance item(Item item);
       }
 
+      @TemplateExtension
+      public class TemplateExtensions {
+
+            public static BigDecimal discountedPrice(Item item) {
+                  return item.getPrice().multiply(new BigDecimal("0.9"));
+            }
+      }
+
       @GET
       @Produces(MediaType.TEXT_HTML)
       public TemplateInstance getHello(@QueryParam("name") String name) {
@@ -33,6 +46,8 @@ public class HelloResource {
       @Path("item/{id}")
       @Produces(MediaType.TEXT_HTML)
       public TemplateInstance getItem(@PathParam("id") String id) {
+            log.info("Fetching item with id: {}", id);
+            log.info("Item details: {}", this.itemService.getItem(id));
             return Templates.item(this.itemService.getItem(id));
       }
 
