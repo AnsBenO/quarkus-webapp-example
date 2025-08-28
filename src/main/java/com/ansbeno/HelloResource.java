f22b791fd1ce6cb819b118568c36800d43f08f68
Anas BenOmar <138506861+AnsBenO@users.noreply.github.com>
2025-08-28 12:28:36 +0100

    created first service

--- src/main/java/com/ansbeno/HelloResource.java
+++ src/main/java/com/ansbeno/HelloResource.java
@@ -4,6 +4,7 @@ import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
@@ -13,15 +14,26 @@ import lombok.RequiredArgsConstructor;
 @Path("/hello")
 public class HelloResource {
 
+      private final ItemService itemService;
+
       @CheckedTemplate
       public static class Templates {
             public static native TemplateInstance hello(String name);
+
+            public static native TemplateInstance item(Item item);
       }
 
       @GET
       @Produces(MediaType.TEXT_HTML)
-      public TemplateInstance get(@QueryParam("name") String name) {
+      public TemplateInstance getHello(@QueryParam("name") String name) {
             return Templates.hello(name);
+      }
+
+      @GET
+      @Path("item/{id}")
+      @Produces(MediaType.TEXT_HTML)
+      public TemplateInstance getItem(@PathParam("id") String id) {
+            return Templates.item(this.itemService.getItem(id));
       }
 
 }
---
+++ src/main/java/com/ansbeno/Item.java
@@ -0,0 +1,19 @@
+package com.ansbeno;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item {
+      private String id;
+      private String name;
+      private BigDecimal price;
+
+}
